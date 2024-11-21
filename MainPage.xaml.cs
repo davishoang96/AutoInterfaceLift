2cdecfef707b74b50347f6c38436e34c2ee7981b
Davis Hoang <davishoang96@gmail.com>
2024-11-21 22:22:29 +0800

    init project files

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoInterfaceLift;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AutoInterfaceLift;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
